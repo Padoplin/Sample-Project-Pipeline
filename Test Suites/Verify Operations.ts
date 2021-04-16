f004a00768685a0d1353bf3b58a2e68787cb7dc6
Padoplin <52145059+Padoplin@users.noreply.github.com>
2021-04-16 17:59:21 +0700

    hi

--- Test Suites/Verify Operations.ts
+++ Test Suites/Verify Operations.ts
@@ -9,6 +9,7 @@
    <pageLoadTimeout>5</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>437cc529-6e28-4c94-ae29-e1709755d800</testSuiteGuid>
    <testCaseLink>
       <guid>665d328f-aff4-4e66-83d3-858388a521f0</guid>
@@ -33,5 +34,29 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/operations/Verify Plus</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e0f5fbd7-9ae7-4126-93ce-f1fb42c15d99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/Verify Multiply</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7afe0940-ba81-4234-ad0e-54d13fd81691</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/Verify Plus</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6299401-dbcd-4563-8ccd-75ac9b5b2cad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/Verify Minus</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e121f52b-1a71-4f26-bb30-1f6c09d999b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/Verify Divide</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
